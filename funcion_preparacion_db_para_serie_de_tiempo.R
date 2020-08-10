076f77704ee570e20ddbba86b8bf723e223ce7e3
unknown <fcomanquehual@gmail.com>
2020-08-10 19:40:06 -0400

    script original

--- funcion_preparacion_db_para_serie_de_tiempo.R
+++ funcion_preparacion_db_para_serie_de_tiempo.R
@@ -1,5 +1,5 @@
 preparacion.db.para.serie.de.tiempo <- function(data_base, anho_para_filtro=NULL){
-  #data_base <- db0
+  # data_base <- db.pp
   
   colnames(data_base)[1] <- 'Date'
   data_base[,1] <- as.character(data_base[,1])
@@ -8,7 +8,8 @@ preparacion.db.para.serie.de.tiempo <- function(data_base, anho_para_filtro=NULL
   data_base$anho_mes <- format(data_base[,1],'%Y-%m')
   data_base$anho <- format(data_base[,1],'%Y')
   
-  if(is.null(anho_para_filtro)){db.out0 <- data_base} else(db.out0 <- subset(data_base, anho==anho_para_filtro))
+  if(is.null(anho_para_filtro)){db.out0 <- data_base} else(c(id <- which(data_base$anho%in%anho_para_filtro), 
+                                                             db.out0 <- data_base[id,]))
   db.out <- db.out0[,-ncol(db.out0)]
   return(db.out)
 }
